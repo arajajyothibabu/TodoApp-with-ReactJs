e1545daf788e463371f312716a4fc8019661ceec
kshatriyaindraja <arajajyothibabu@gmail.com>
2016-08-15 19:01:42 +0530

    initial stage of 3 states

--- js/app.jsx
+++ js/app.jsx
@@ -21,13 +21,13 @@ var FilterComponent =React.createClass({
         return(
             <ul className="nav nav-pills">
                 <li role="presentation" className={ Boolean(this.state.activeArray[0]) ? "active" : "" } ><a href="#" id="0" onClick={this.handleFilter} >
-                    All Tasks
+                    New Tasks
                 </a></li>
                 <li role="presentation" className={ Boolean(this.state.activeArray[1])? "active" : "" } ><a href="#" id="1" onClick={this.handleFilter }>
-                    Completed Tasks
+                    OnGoing Tasks
                 </a></li>
                 <li role="presentation" className={ Boolean(this.state.activeArray[2]) ? "active" : ""} ><a href="#" id="2" onClick={this.handleFilter}>
-                    InCompleted Tasks
+                    Completed Tasks
                 </a></li>
             </ul>
         );
@@ -44,8 +44,9 @@ var AddComponent = React.createClass({
         this.setState({ desc : ""});
     },
     handleTask : function (e) {
-        if(e.target.value == "") return;
         this.setState({ desc : e.target.value});
+        if(e.target.value == "") return;
+
     },
    render:function(){
        return(
@@ -89,24 +90,41 @@ var ListComponent = React.createClass({
         e.preventDefault();
         this.props.handleRemove(e.target.id);
     },
+    handleOnGoing : function(e)
+    {
+        e.preventDefault();
+        this.props.handleOnGoing(e.target.id);
+    },
     render : function() {
         var _this = this;
         return (
             <div className="list-group">
                 {
                     this.state.todoList.map(function (task) {
-                        var classes = task.status? classes = 'list-group-item clearfix list-group-item-success' : 'list-group-item clearfix';
+                        var classes = task.statusList.value==0? classes = 'list-group-item clearfix list-group-item-success' : 'list-group-item clearfix';
                         return (
                             <li className={classes} id={task.id}>
                                 {
                                     task.desc
                                 }
+                               <div className="pull-right" role="group">
+                                   {
+                                       task.statusList.value == 0 || <button type="button" id={task.id} className="btn btn-xs btn-success img-circle" onClick={_this.handleComplete}>New</button>
+                                   }
+                               </div>
                                 <div className="pull-right" role="group">
                                     {
-                                        Boolean(task.status) || <button type="button" id={task.id} className="btn btn-xs btn-success img-circle" onClick={_this.handleComplete}>&#x2713;</button>
+                                        task.statusList.value == 1 || <button type="button" id={task.id} className="btn btn-xs btn-danger img-circle" onClick={_this.handleOnGoing}>OnGoing</button>
+
                                     }
-                                     &nbsp;<button type="button" id={task.id} className="btn btn-xs btn-danger img-circle" onClick={_this.removeTask}>&#xff38;</button>
                                 </div>
+                                <div className="pull-right" role="group">
+                                    {
+                                        task.statusList.value == 2 || <button type="button" id={task.id} className="btn btn-xs btn-warning img-circle" onClick={_this.removeTask}>Completed</button>
+
+                                    }
+                                </div>
+
                             </li>
                         );
                     })
@@ -122,19 +140,20 @@ var TodoAppComponent = React.createClass({
     },
     getInitialState: function () {
         return {
-            todoList : [],
+            todoList : [{id : this.generateId(), desc: 'eat food', statusList:0},{id : this.generateId(), desc: 'eat food regularly', statusList :1}] ,
             filteredList : [],
-            currentFilter : 0
+            currentFilter : 0,
+            statusList :[0,1,2]
         };
     },
-   
+
     componentWillMount : function () {
         this.setState({
             filteredList : this.state.todoList
         });
     },
     addTodoTask : function (task) {
-        var newTask = { id : this.generateId(), desc: task, status : false};
+        var newTask = { id : this.generateId(), desc: task, statusList :0 };
         this.setState({ todoList : this.state.todoList.concat([newTask])}, function(){
             this.addTodoFilter(this.state.currentFilter);
         });
@@ -143,7 +162,7 @@ var TodoAppComponent = React.createClass({
         this.setState({
             currentFilter : filter,
             filteredList : this.state.todoList.filter(function (task, i) {
-                return filter == 0 || (filter == 1 && task.status) || (filter == 2 && !task.status);
+                return filter == 0 || (filter == 1 && (task.statusList.value==1)) || (filter == 2 && (task.statusList.value==2));
             })
         });
     },
@@ -160,7 +179,20 @@ var TodoAppComponent = React.createClass({
         this.setState({
             todoList : this.state.todoList.map(function (task, i) {
                 if(taskId == task.id){
-                    return {id : task.id, desc : task.desc, status : true };
+                    return {id : task.id, desc : task.desc, statusList : 2 };
+                }else{
+                    return task;
+                }
+            })
+        }, function(){
+            this.addTodoFilter(this.state.currentFilter);
+        });
+    },
+    handleOnGoing : function(taskId){
+        this.setState({
+            todoList : this.state.todoList.map(function (task, i) {
+                if(taskId == task.id){
+                    return {id : task.id, desc : task.desc, statusList : 1 };
                 }else{
                     return task;
                 }
@@ -176,7 +208,7 @@ var TodoAppComponent = React.createClass({
                 <hr />
                 <FilterComponent addTodoFilter={this.addTodoFilter} />
                 <hr />
-                <ListComponent list={ this.state.filteredList } handleRemove={this.handleRemove} handleComplete={this.handleComplete}/>
+                <ListComponent list={ this.state.filteredList } handleRemove={this.handleRemove} handleComplete={this.handleComplete} handleOnGoing={this.handleOnGoing}/>
             </div>
         );
     }
